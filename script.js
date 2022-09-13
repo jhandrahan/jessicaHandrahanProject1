94ed63dadb90091f9f29b2590ec2c0ae6aca1fbe
jhandrahan <jessicahandrahan@gmail.com>
2022-09-13 20:28:31 -0300

    Added Javascript to make a hamburger menu for mobile size

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", ()=>{
+    const hamburgerButton = document.querySelector('#hamburger');
+    const closeHamburgerButton = document.querySelector('.closeMenu');
+    const hamburgerMenu = document.querySelector('.slideOutNav');
+
+    hamburgerButton.addEventListener('click', (event)=>{
+        hamburgerMenu.classList.add('open');   
+    });
+
+    closeHamburgerButton.addEventListener('click', (event) =>{
+        hamburgerMenu.classList.remove('open');
+    });
+})
